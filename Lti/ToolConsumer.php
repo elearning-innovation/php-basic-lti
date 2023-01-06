db69a2d79bc701a2b204f4b52dd90ed82f001f88
John Cornell <github@jrcii.com>
2023-01-06 18:27:35 -0500

    Update PHP style

--- Lti/ToolConsumer.php
+++ Lti/ToolConsumer.php
@@ -9,91 +9,118 @@ namespace Oscelot\Lti;
  */
 class ToolConsumer
 {
-
     /**
      * Local name of tool consumer.
      */
-    public $name = null;
+    public ?string $name = null;
+
     /**
      * Shared secret.
      */
-    public $secret = null;
+    public ?string $secret = null;
+
     /**
      * LTI version (as reported by last tool consumer connection).
      */
-    public $lti_version = null;
+    public ?string $lti_version = null;
+
     /**
      * Name of tool consumer (as reported by last tool consumer connection).
      */
-    public $consumer_name = null;
+    public ?string $consumer_name = null;
+
     /**
      * Tool consumer version (as reported by last tool consumer connection).
      */
-    public $consumer_version = null;
+    public ?string $consumer_version = null;
+
     /**
      * Tool consumer GUID (as reported by first tool consumer connection).
      */
-    public $consumer_guid = null;
+    public ?string $consumer_guid = null;
+
     /**
      * Optional CSS path (as reported by last tool consumer connection).
      */
-    public $css_path = null;
+    public ?string $css_path = null;
+
     /**
-     * True if the tool consumer instance is protected by matching the consumer_guid value in incoming requests.
+     * True if the tool consumer instance is protected by matching the
+     * consumer_guid value in incoming requests.
      */
-    public $protected = false;
+    public bool $protected = false;
+
     /**
-     * True if the tool consumer instance is enabled to accept incoming connection requests.
+     * True if the tool consumer instance is enabled to accept incoming
+     * connection requests.
      */
-    public $enabled = false;
+    public bool $enabled = false;
+
     /**
-     * Date/time from which the the tool consumer instance is enabled to accept incoming connection requests.
+     * Date/time from which the the tool consumer instance is enabled to accept
+     * incoming connection requests.
      */
-    public $enable_from = null;
+    public ?string $enable_from = null;
+
     /**
-     * Date/time until which the tool consumer instance is enabled to accept incoming connection requests.
+     * Date/time until which the tool consumer instance is enabled to accept
+     * incoming connection requests.
      */
-    public $enable_until = null;
+    public ?string $enable_until = null;
+
     /**
      * Date of last connection from this tool consumer.
      */
-    public $last_access = null;
+    public ?string $last_access = null;
+
     /**
      * Default scope to use when generating an Id value for a user.
      */
-    public $id_scope = ToolProvider::ID_SCOPE_ID_ONLY;
+    public int $id_scope = ToolProvider::ID_SCOPE_ID_ONLY;
+
     /**
-     * Default email address (or email domain) to use when no email address is provided for a user.
+     * Default email address (or email domain) to use when no email address is
+     * provided for a user.
      */
-    public $defaultEmail = '';
+    public string $defaultEmail = '';
+
     /**
      * Date/time when the object was created.
      */
-    public $created = null;
+    public ?string $created = null;
+
     /**
      * Date/time when the object was last updated.
      */
-    public $updated = null;
+    public ?string $updated = null;
 
     /**
      * Consumer key value.
      */
-    private $key = null;
+    private ?string $key = null;
+
     /**
      * Data connector object or string.
      */
-    private $data_connector = null;
+    private ?AbstractDataConnector $data_connector;
 
     /**
      * Class constructor.
      *
-     * @param string  $key             Consumer key
-     * @param mixed   $data_connector  String containing table name prefix, or database connection object, or array containing one or both values (optional, default is MySQL with an empty table name prefix)
-     * @param bool $autoEnable      true if the tool consumers is to be enabled automatically (optional, default is false)
+     * @param ?string $key Consumer key.
+     * @param string $data_connector String containing table name prefix, or
+     *                              database connection object, or array
+     *                              containing one or both values (optional,
+     *                              default is MySQL with an empty table name
+     *                              prefix).
+     * @param bool $autoEnable      true if the tool consumers is to be enabled
+     *                              automatically (optional, default is false).
      */
-    public function __construct($key = null, $data_connector = '', $autoEnable = false)
-    {
-
+    public function __construct(
+        ?string $key = null,
+        string $data_connector = '',
+        bool $autoEnable = false
+    ) {
         $this->data_connector = AbstractDataConnector::getDataConnector($data_connector);
         if (!empty($key)) {
             $this->load($key, $autoEnable);
@@ -105,9 +132,8 @@ class ToolConsumer
     /**
      * Initialise the tool consumer.
      */
-    public function initialise()
+    public function initialise(): void
     {
-
         $this->key = null;
         $this->name = null;
         $this->secret = null;
@@ -130,55 +156,51 @@ class ToolConsumer
     /**
      * Save the tool consumer to the database.
      *
-     * @return bool True if the object was successfully saved
+     * @return bool True if the object was successfully saved.
      */
-    public function save()
+    public function save(): bool
     {
-
         return $this->data_connector->Tool_Consumer_save($this);
     }
 
     /**
      * Delete the tool consumer from the database.
      *
-     * @return bool True if the object was successfully deleted
+     * @return bool True if the object was successfully deleted.
      */
-    public function delete()
+    public function delete(): bool
     {
-
         return $this->data_connector->Tool_Consumer_delete($this);
     }
 
     /**
      * Get the tool consumer key.
      *
-     * @return string Consumer key value
+     * @return ?string Consumer key value.
      */
-    public function getKey()
+    public function getKey(): ?string
     {
-
         return $this->key;
     }
 
     /**
      * Get the data connector.
      *
-     * @return mixed Data connector object or string
+     * @return AbstractDataConnector Data connector object or string.
      */
-    public function getDataConnector()
+    public function getDataConnector(): AbstractDataConnector
     {
-
         return $this->data_connector;
     }
 
     /**
      * Is the consumer key available to accept launch requests?
      *
-     * @return bool True if the consumer key is enabled and within any date constraints
+     * @return bool True if the consumer key is enabled and within any date
+     *              constraints.
      */
-    public function getIsAvailable()
+    public function getIsAvailable(): bool
     {
-
         $ok = $this->enabled;
 
         $now = time();
@@ -192,28 +214,21 @@ class ToolConsumer
         return $ok;
     }
 
-###
-###  PRIVATE METHOD
-###
-
     /**
      * Load the tool consumer from the database.
      *
-     * @param string  $key        The consumer key value
-     * @param bool $autoEnable True if the consumer should be enabled (optional, default if false)
-     *
-     * @return bool True if the consumer was successfully loaded
+     * @param string $key      The consumer key value.
+     * @param bool $autoEnable True if the consumer should be enabled
+     *                         (optional, default if false).
+     * @return void True if the consumer was successfully loaded.
      */
-    private function load($key, $autoEnable = false)
+    private function load(string $key, bool $autoEnable = false): void
     {
-
         $this->initialise();
         $this->key = $key;
         $ok = $this->data_connector->Tool_Consumer_load($this);
         if (!$ok) {
             $this->enabled = $autoEnable;
         }
-
-        return $ok;
     }
 }
