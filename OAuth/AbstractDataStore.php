1bdfaf71db745d9eb6767039ad83a8b9f8873785
John Cornell <github@jrcii.com>
2023-01-06 17:49:50 -0500

    Update PHP style

--- OAuth/AbstractDataStore.php
+++ OAuth/AbstractDataStore.php
@@ -7,37 +7,45 @@ namespace Oscelot\OAuth;
 use Oscelot\Lti\ConsumerNonce;
 use Oscelot\Lti\ToolConsumer;
 
-class AbstractDataStore
+abstract class AbstractDataStore
 {
-    function lookup_consumer(string $consumer_key)
-    {
-        // implement me
-    }
+    abstract public function lookup_consumer(string $consumer_key);
 
-    function lookup_token(ToolConsumer $consumer, string $token_type, string $token)
-    {
-        // implement me
-    }
+    abstract public function lookup_token(
+        ToolConsumer $consumer,
+        string $token_type,
+        string $token
+    );
 
-    function lookup_nonce(
+    abstract public function lookup_nonce(
         ToolConsumer $consumer,
         string $token,
         ConsumerNonce $nonce,
         string $timestamp
-    ) {
-        // implement me
-    }
+    );
 
-    function new_request_token(ToolConsumer $consumer, $callback = null): Token
-    {
-        // return a new token attached to this consumer
-    }
+    /**
+     * @param ToolConsumer $consumer
+     * @param              $callback
+     * @return Token A new token attached to this consumer
+     */
+    abstract public function new_request_token(
+        ToolConsumer $consumer,
+        $callback = null
+    ): Token;
 
-    function new_access_token(Token $token, ToolConsumer $consumer, $verifier = null)
-    {
-        // return a new access token attached to this consumer
-        // for the user associated with this token if the request token
-        // is authorized
-        // should also invalidate the request token
-    }
+    /**
+     * @param Token        $token
+     * @param ToolConsumer $consumer
+     * @param              $verifier
+     *
+     * @return Token A new access token attached to this consumer for the user
+     *               associated with this token if the request token is
+     *               authorized should also invalidate the request token.
+     */
+    abstract public function new_access_token(
+        Token $token,
+        ToolConsumer $consumer,
+        $verifier = null
+    ): Token;
 }
